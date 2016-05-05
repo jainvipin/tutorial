a2de99d3c3d90d44cabeacd8e3f3a111400d8276
Vipin Jain <vipijain@insiemenetworks.com>
2016-05-04 23:22:15 -0700

    initial commit

---
+++ bin/cleanup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# sanity tests
+import api.tnode
+import time
+import sys
+import argparse
+
+# Parse command line args
+# Create the parser and sub parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', action='version', version='1.0.0')
+parser.add_argument("-nodes", required=True, help="list of nodes(comma seperated)")
+parser.add_argument("-user", default='vagrant', help="User id for ssh")
+parser.add_argument("-password", default='vagrant', help="password for ssh")
+
+# Parse the args
+args = parser.parse_args()
+addrList = args.nodes.split(",")
+
+nodes = []
+
+# Cleanup nodes
+for addr in addrList:
+	node = api.tnode.Node(addr, args.user, args.password)
+	node.cleanupContainers()
+	nodes.append(node)
+
+for node in nodes:
+	# Cleanup all state before we can start
+	node.stopNetmaster()
+	node.cleanupDockerNetwork()
+	node.stopNetplugin()
+	node.cleanupMaster()
+	node.cleanupSlave()
---
+++ bin/startPlugin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Start netplugin and netmaster
+import tbed
+import time
+import sys
+import os
+import argparse
+
+# Parse command line args
+# Create the parser and sub parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', action='version', version='1.0.0')
+parser.add_argument("-nodes", required=True, help="list of nodes(comma seperated)")
+parser.add_argument("-user", default='vagrant', help="User id for ssh")
+parser.add_argument("-password", default='vagrant', help="password for ssh")
+parser.add_argument("-binpath", default='/opt/gopath/bin', help="netplugin/netmaster binary path")
+
+# Parse the args
+args = parser.parse_args()
+addrList = args.nodes.split(",")
+
+# Cleanup all state and start netplugin/netmaster
+testbed = api.tbed.Testbed(addrList, args.user, args.password, args.binpath)
+
+print "Waiting for netmaster to come up"
+time.sleep(15)
+
+# Setup proxy
+# setupProxy.setupProxy()
+
+print "################### Started Netplugin #####################"
+os._exit(0)
